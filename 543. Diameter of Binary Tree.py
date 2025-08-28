b2fb8e7cbe435217281a9210114fa9d09fd0149a
Vladimir Yakimenko <thevladimiryakimenko@gmail.com>
2025-08-28 17:08:54 +0300

    100. Same Tree

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -7,22 +7,12 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.diameter = 0
-        self.cache = {}
 
         def depth(node):
             if not node:
                 return 0
-            
-            key_left = (node.left,)
-            key_right = (node.right,)
-
-            if not self.cache.get(key_left):
-                self.cache[key_left] = depth(node.left)
-            if not self.cache.get(key_right):
-                self.cache[key_right] = depth(node.right)
-
-            left = self.cache.get(key_left)
-            right = self.cache.get(key_right)
+            left = depth(node.left)
+            right = depth(node.right)
 
             self.diameter = max(self.diameter, left + right)
 
